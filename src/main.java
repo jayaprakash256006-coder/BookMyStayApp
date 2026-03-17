ad7fe1dcde25f7c316ea4f92ae47f2f0dc6c4838
jayaprakash256006-coder <jayaprakash256006@gmail.com>
2026-03-17 14:40:38 +0530

    main

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Book My Stay App ===");
+        System.out.println("Welcome to Hotel Booking System");
+        System.out.println("System initialized successfully");
+
+    }
+}
